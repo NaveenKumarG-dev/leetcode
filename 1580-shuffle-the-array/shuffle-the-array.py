7ced59e2cde7c7ac070623106b802889422a4de2
Naveen Kumar G <naveen13524g@gmail.com>
2026-08-03 10:19:20 +0530

    Time: 39 ms (67.35%) | Memory: 12.5 MB (81.64%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+
+        shuffled = []
+
+        for i in range(n):
+            shuffled.append(nums[i])
+            shuffled.append(nums[i+n])
+        
+        return shuffled
