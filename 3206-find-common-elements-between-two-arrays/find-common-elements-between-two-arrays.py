522abdb4ac90b6b0da7c8548c91edd79effe4bb5
Naveen Kumar G <naveen13524g@gmail.com>
2026-09-16 10:41:54 +0530

    Time: 4 ms (86.98%) | Memory: 19.4 MB (26.24%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.py
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.py
@@ -5,10 +5,10 @@ class Solution:
         answer1=0
         answer2=0
         for i in nums1:
-            if i in nums2:
+            if i in set2:
                 answer1+=1
         for i in nums2:
-            if i in nums1:
+            if i in set1:
                 answer2+=1
         
         return [answer1,answer2]
